08ea6b3680aa6f45b9a9cb2328500ed4a51ff7cc
pythonlearner1025 <mjsong2021@gmail.com>
2023-12-19 15:24:33 -0500

    tpu wrapped

--- bark/generation.py
+++ bark/generation.py
@@ -245,7 +245,7 @@ def _load_model(ckpt_path, device, use_small=False, model_type="text"):
     val_loss = checkpoint["best_val_loss"].item()
     logger.info(f"model loaded: {round(n_params/1e6,1)}M params, {round(val_loss,3)} loss")
     model.eval()
-    model = xmp.MpModelWrapper(model)
+    #model = xmp.MpModelWrapper(model)
     model.to(device)
     del checkpoint, state_dict
     _clear_cuda_cache()
@@ -262,7 +262,7 @@ def _load_codec_model(device):
     model = EncodecModel.encodec_model_24khz()
     model.set_target_bandwidth(6.0)
     model.eval()
-    model = xmp.MpModelWrapper(model)
+    #model = xmp.MpModelWrapper(model)
     model.to(device)
     _clear_cuda_cache()
     return model
